f7bf03eda01eba5e9b0cb38e6bcfb060783a7b7a
Dan Kahle <dan.kahle@gmail.com>
2014-06-13 20:27:38 -0700

    setup site one under web proj

---
+++ README.md
@@ -0,0 +1,3 @@
+##cloud-one
+
+This is a sub repo for the cloud repo. That repo is a test to see if we can webapi at a site root for serivces rendered to multiple aggregated sub angular sites. This is an attempt to pay for one azure website that in actuality houses many (it's all about the benjamins).
---
+++ app/js/app.js
@@ -0,0 +1,29 @@
+/// <reference path="../bower_components/angular/angular.js" />
+"use strict";
+
+var app = angular.module("app", []);
+
+app.controller("ctrl", function ($scope, $http) {
+
+/*
+	$scope.users = [
+		{ name: "dank", age: 50 },
+		{ name: "carl", age: 60 }
+	];
+*/
+
+	$scope.users = [];
+
+	$http.get("http://localhost:3404/api/User").success(function (data) {
+		$scope.users = JSON.parse(data);
+		
+		console.log("data", data)
+		console.log("data", $scope.users[0]);
+	});
+
+});
+
+
+
+
+
